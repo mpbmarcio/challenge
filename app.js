272173af270699ba5ac9d5f289a14daa51bd66ca
Márcio Pires Barboza <mpbmarcio@hotmail.com>
2025-03-11 10:13:50 -0300

    primeiro commit (estrutura)

---
+++ app.js
@@ -0,0 +1,29 @@
+let amigos = [];
+let ul = document.getElementById('listaAmigos');
+let result = document.getElementById('resultado');
+let nome = document.getElementById("amigo");
+
+function adicionarAmigo() {
+
+};
+
+function validarNome(nome) {
+
+};
+
+function exibirListaNomes(lista) {    
+
+};
+
+function sortearAmigo() {
+
+};
+
+function reiniciarSorteio() {
+
+}
+
+function limparCampo(campo) {
+    campo.value = '';
+    campo.focus();
+};
